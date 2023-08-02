33413ea3d1ac25dd53db47f5825329705ef0887a
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-08-02 12:49:39 +0530

    Added: BCrypt Password.

--- src/main/java/com/naman/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/naman/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -37,13 +38,17 @@ public class BasicAuthSecurityConfiguration {
     public UserDetailsService userDetailService(DataSource dataSource) {
 
         var user = User.withUsername("naman")
-                .password("{noop}dummy")
+//                .password("{noop}dummy")
+                .password("dummy")
+                .passwordEncoder(s -> passwordEncoder().encode(s))
                 .roles("USER")
                 .build();
 
 
         var admin = User.withUsername("admin")
-                .password("{noop}dummy")
+//                .password("{noop}dummy")
+                .password("dummy")
+                .passwordEncoder(s -> passwordEncoder().encode(s))
                 .roles("ADMIN")
                 .build();
 
@@ -60,5 +65,10 @@ public class BasicAuthSecurityConfiguration {
                 .setType(EmbeddedDatabaseType.H2)
                 .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
                 .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
