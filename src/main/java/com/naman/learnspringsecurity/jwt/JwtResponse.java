a82cb39594056c1217793cbcf4ba4f8e76e744d3
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-08-02 15:36:31 +0530

    Added: DOne auth with response.

---
+++ src/main/java/com/naman/learnspringsecurity/jwt/JwtResponse.java
@@ -0,0 +1,21 @@
+package com.naman.learnspringsecurity.jwt;
+
+public class JwtResponse {
+
+    private String token;
+
+    public JwtResponse() {
+    }
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
